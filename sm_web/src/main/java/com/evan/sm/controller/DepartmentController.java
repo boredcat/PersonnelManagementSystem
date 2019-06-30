ba6fdea7ca33c6b48d53e9c08abd2910dc3c8a46
EvanZhao <phzhao7548@gmail.com>
2019-06-30 18:44:48 +0800

    查询部门功能

---
+++ sm_web/src/main/java/com/evan/sm/controller/DepartmentController.java
@@ -0,0 +1,25 @@
+package com.evan.sm.controller;
+
+import com.evan.sm.entity.Department;
+import com.evan.sm.service.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@Controller("departmentController")
+public class DepartmentController {
+
+    @Autowired
+    private DepartmentService departmentService;
+
+    public void list(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Department> list = departmentService.getAll();
+        request.setAttribute("LIST",list);
+        request.getRequestDispatcher("../department_list.jsp").forward(request,response);
+    }
+}
